83abf9576bf2485fb6cc50376c818837ebebdf68
Joe Fang <25814618+MinecraftFuns@users.noreply.github.com>
2024-10-17 14:30:56 -0400

    chore(BlockedAppPage): initialize blocked page

--- app/src/main/java/com/example/focusflow/App.kt
+++ app/src/main/java/com/example/focusflow/App.kt
@@ -3,5 +3,6 @@ package com.example.focusflow
 data class App(
     var active: Boolean = false,
     val logo: Int = 0,
-    val name: String = ""
+    val name: String = "", // a user-recognizable name
+    val packageName: String = "" // the underlying package name
 )
